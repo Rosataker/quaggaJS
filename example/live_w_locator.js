bfec33b496c5b3e953b46286b4587caaf2540729
Rosataker <pechin1219@gmail.com>
2018-08-30 09:34:34 +0800

    Update live_w_locator.js

--- example/live_w_locator.js
+++ example/live_w_locator.js
@@ -262,11 +262,11 @@ $(function() {
         var drawingCtx = Quagga.canvas.ctx.overlay,
             drawingCanvas = Quagga.canvas.dom.overlay;
 
-        if(timekeep == 50){
+        if(timekeep == 100){
             var readers_change = ['code_128_reader', 'code_39', 'code_93_reader'];
             var state = App._convertNameToState("decoder_readers");
-            alert("readers_change->" +readers_change[now_readers_flag]);
-            //App.setState(state,readers_change[now_readers_flag]);
+            //alert("readers_change->" +readers_change[now_readers_flag]);
+            App.setState(state,readers_change[now_readers_flag]);
             timekeep = 0;
             now_readers_flag++;
             if(now_readers_flag % 3 == 0){
