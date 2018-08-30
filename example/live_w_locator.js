c63920bbe997f732c68f2efd2df54bf2f1b6373b
Rosataker <pechin1219@gmail.com>
2018-08-30 09:37:08 +0800

    Update live_w_locator.js

--- example/live_w_locator.js
+++ example/live_w_locator.js
@@ -265,7 +265,7 @@ $(function() {
         if(timekeep == 100){
             var readers_change = ['code_128_reader', 'code_39', 'code_93_reader'];
             var state = App._convertNameToState("decoder_readers");
-            //alert("readers_change->" +readers_change[now_readers_flag]);
+
             App.setState(state,readers_change[now_readers_flag]);
             timekeep = 0;
             now_readers_flag++;
